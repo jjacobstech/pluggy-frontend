cc2920888badb903abc1b144983810264c1c4f92
Jacobs Joshua <jacobsjoshua81@gmail.com>
2025-10-05 23:07:24 +0100

    updates

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import { APP_NAME } from './config'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router)z
 
 app.mount('#app')
-const title: HTMLElement|null = document.getElementById('title')
-title.innerHTML = APP_NAME
