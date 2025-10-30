69574123c8d962d478b08d2debb6f34b13a84b11
Jacobs Joshua <jacobsjoshua81@gmail.com>
2025-10-30 08:36:56 +0100

    CI

--- src/components/home/Footer.vue
+++ src/components/home/Footer.vue
@@ -4,8 +4,8 @@
             <div class="container mx-auto px-4 py-8">
                   <div class="flex flex-col md:flex-row items-center justify-between gap-4">
                         <div class="flex items-center gap-2">
-                              <div class="w-6 h-6 rounded-lg bg-gradient-primary flex items-center justify-center">
-                                    <LucidePlug class="w-5 h-5 text-black" color="black" />
+                              <div class="w-6 h-6 rounded-lg flex items-center justify-center">
+                                    <LucidePlug class="w-5 h-5 text-white" color="black" />
                               </div>
                               <span class="font-bold text-lg font-lobster-bold">Pluggy.io</span>
                         </div>
